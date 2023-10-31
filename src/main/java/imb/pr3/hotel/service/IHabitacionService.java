3e7dfee1e1e9833dd34d00d1984beffde6b0e337
CarPes <pesacarlos1995@gmail.com>
2023-10-31 14:50:01 -0300

    Elimnar archivos no deseados

--- src/main/java/imb/pr3/hotel/service/IHabitacionService.java
+++ src/main/java/imb/pr3/hotel/service/IHabitacionService.java
@@ -9,7 +9,7 @@ public interface IHabitacionService {
 
 	public List<Habitacion> obtenerTodasLasHabitacion(); //Obtiente todas las habitaciones de la bbdd
     public Habitacion buscarHabitacionPorId(Long id); //Obtiene una habitacion por su ID
-    public Habitacion crearHabitacion(Habitacion habitacion); //Crea una neva habitacion en la bbdd
+    public Habitacion crearHabitacion(Habitacion habitacion); //Crea una neva habitacion en la bbdd.
     public String eliminarHabitacion(Long id); //Eliminar una habitacion por ID
     public Habitacion modificarHabitacion(Habitacion habitacion); //Modifca los datos de la habitacion en la bbdd
 }
