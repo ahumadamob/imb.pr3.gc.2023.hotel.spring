56767f23a35acb4c0998abc9655ecbc37a6f4609
Arrigo <arrigo.z.97@gmail.com>
2023-12-11 21:45:49 -0300

    Probando commit desde vsc

--- src/main/java/imb/pr3/hotel/repository/ClienteRepository.java
+++ src/main/java/imb/pr3/hotel/repository/ClienteRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import imb.pr3.hotel.entity.Cliente;
-
+//Se encarga de trabajar con la base de datos, interactúa usando métodos traídos de jpa
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	public List<Cliente>findByHabilitado(boolean habilitado);
 }
